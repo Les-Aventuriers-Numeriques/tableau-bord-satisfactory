781f49ba9949554481ad71515f2a68ad13abd8bc
EpocDotFr <contact@epoc.fr>
2024-07-25 10:50:47 +0200

    Electricité

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@
             intervalId: null,
             sounds: {},
 
-            init() {
+            actuallyInit() {
                 this.loadSounds()
 
                 if (this.refresh(true)) {
@@ -67,6 +67,10 @@
                 players: [],
                 soundsToLoad: ['motus-boule-noire'],
 
+                init() {
+                    this.actuallyInit()
+                },
+
                 refresh(firstTime = false) {
                     const self = this
 
@@ -108,6 +112,77 @@
                             })
 
                         // self.playSound('motus-boule-noire') quand un joueur meurt
+                    })
+
+                    return true
+                }
+            })
+        })
+
+        Alpine.data('powerChartComponent', function () {
+            return Object.assign(Object.create(baseComponent), {
+                data: [
+                    [], // Timestamps
+                    [], // Conso
+                    [], // Prod
+                    [], // Capacité
+                    [], // Conso max
+                ],
+                uPlotInstance: null,
+                uPlotOptions: {
+                    width: '1200',
+                    height: '500',
+                    series: [
+                        {
+                            label: 'Consommation',
+                            stroke: 'red',
+                            width: 1
+                        },
+                        {
+                            label: 'Production',
+                            stroke: 'green',
+                            width: 1
+                        },
+                        {
+                            label: 'Capacité',
+                            stroke: 'blue',
+                            width: 1
+                        },
+                        {
+                            label: 'Consommation max',
+                            stroke: 'orange',
+                            width: 1
+                        }
+                    ]
+                },
+
+                init() {
+                    this.uPlotInstance = new uPlot(this.uPlotOptions, this.data, this.$el)
+
+                    this.actuallyInit()
+                },
+
+                refresh(firstTime = false) {
+                    const self = this
+
+                    this.fetch('getPower').then(function (powerCircuits) {
+                        self.data[0].push(Date.now() / 1000)
+
+                        const attrs = ['PowerConsumed', 'PowerProduction', 'PowerCapacity', 'PowerMaxConsumed']
+
+                        attrs.forEach(function (attr, index) {
+                            self.data[index + 1].push(
+                                powerCircuits
+                                    .map(function (powerCircuit) {
+                                        return powerCircuit[attr]
+                                    })
+                                    .reduce(function (a, b) {
+                                        return a + b
+                                    })
+                            )
+                        })
+
+                        self.uPlotInstance.setData(self.data)
                     })
 
                     return true
